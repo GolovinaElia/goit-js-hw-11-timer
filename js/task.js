ee8fe97ab8f0f00decb92dbacce52c4ff47e3ae0
GolovinaElia <eliagolovina@ukr.net>
2021-03-11 23:00:25 +0200

    Добавляет файлы

--- js/task.js
+++ js/task.js
@@ -1,12 +1,12 @@
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector,
-      this.targetDate = targetDate,
+    this.targetDate = targetDate,
     this.intervalId = null
   }
     intervalId = setInterval(function () {
-    let now = new Date().getTime();
-    let time = new Date("Jul 17, 2021") - now;
+    const date = new Date();
+    let time = new Date("Jul 17, 2021") - date;
     
     if (time >= 0) {
       const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -14,11 +14,12 @@ class CountdownTimer {
         Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
       const mins = (Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
       const secs = (Math.floor((time % (1000 * 60)) / 1000));
-    
-      document.querySelector('.value[data-value="days"]').innerHTML = days;
-      document.querySelector('.value[data-value="hours"]').innerHTML = ("0"+hours).slice(-2);
-      document.querySelector('.value[data-value="mins"]').innerHTML = ("0"+mins).slice(-2);
-      document.querySelector('.value[data-value="secs"]').innerHTML = ("0"+secs).slice(-2);
+
+      const timer = document.querySelector('#timer-1');
+      timer.querySelector('.value[data-value="days"]').innerHTML = days;
+      timer.querySelector('.value[data-value="hours"]').innerHTML = ("0"+hours).slice(-2);
+      timer.querySelector('.value[data-value="mins"]').innerHTML = ("0"+mins).slice(-2);
+      timer.querySelector('.value[data-value="secs"]').innerHTML = ("0"+secs).slice(-2);
     } else {
       clearInterval(this.intervalId);
     }
