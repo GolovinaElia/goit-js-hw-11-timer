dba0eaf0395b8af3c70704d64e91e712a1235e4b
GolovinaElia <eliagolovina@ukr.net>
2021-03-14 20:10:19 +0200

    Добавляет файлы

--- js/task.js
+++ js/task.js
@@ -1,31 +1,34 @@
 class CountdownTimer {
   constructor({ selector, targetDate }) {
-    this.selector = selector,
-    this.targetDate = targetDate,
-    this.intervalId = null
-  }
-    intervalId = setInterval(function () {
-    const date = new Date();
-    let time = new Date("Jul 17, 2021") - date;
-    
-    if (time >= 0) {
-      const days = Math.floor(time / (1000 * 60 * 60 * 24));
-      const hours = (
-        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      const mins = (Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      const secs = (Math.floor((time % (1000 * 60)) / 1000));
+    this.targetDate = new Date(targetDate);
+    this.intervalId = null,
+    this.days = document.querySelector(`${selector} .value[data-value="days"]`);
+    this.hours = document.querySelector(`${selector} .value[data-value="hours"]`);
+    this.minutes = document.querySelector(`${selector} .value[data-value="mins"]`);
+    this.seconds = document.querySelector(`${selector} .value[data-value="secs"]`);
 
-      const timer = document.querySelector('#timer-1');
-      timer.querySelector('.value[data-value="days"]').innerHTML = days;
-      timer.querySelector('.value[data-value="hours"]').innerHTML = ("0"+hours).slice(-2);
-      timer.querySelector('.value[data-value="mins"]').innerHTML = ("0"+mins).slice(-2);
-      timer.querySelector('.value[data-value="secs"]').innerHTML = ("0"+secs).slice(-2);
-    } else {
-      clearInterval(this.intervalId);
-    }
-  }, 1000);
-};
+  }
+  
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  timer() {
+    setInterval(() => {
+      const date = new Date();
+        const time = this.targetDate - date;
+      if (time >= 0) {
+        this.days.innerHTML = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        this.hours.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        this.minutes.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        this.seconds.innerHTML = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+      } else {
+        clearInterval(this.intervalId);
+      }
+      }, 1000);
+  }
+}
 new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
-});
+   selector: "#timer-1",
+   targetDate: new Date('Jul 17, 2021'),
+ }).timer();
